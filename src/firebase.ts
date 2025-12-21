17d91feda113d8172b6786c07dfe33937e8f0a01
gabrielfrezouls-hue <gabriel.frezouls@gmail.com>
2025-12-21 16:05:05 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; // Ajoute ça
+import { getFirestore } from "firebase/firestore"; // Ajoute ça
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY, // Utilise import.meta.env
+  // On demande à l'application d'aller chercher la clé dans les secrets GitHub
+  apiKey: import.meta.env.VITE_API_KEY, 
   authDomain: "chaud-devant-81afb.firebaseapp.com",
   projectId: "chaud-devant-81afb",
   storageBucket: "chaud-devant-81afb.firebasestorage.app",
-  messagingSenderId: "774443332221", // Vérifie ce numéro dans tes paramètres Firebase
-  appId: "1:774443332221:web:xxxxxxxxxxxx" // Vérifie cet ID dans tes paramètres Firebase
+  messagingSenderId: "336348032772",
+  appId: "1:336348032772:web:0a92a5c11df89f8b2e6a51",
+  measurementId: "G-J1GHZ52KP3"
 };
+
+// Initialisation
+const app = initializeApp(firebaseConfig);
+
+// EXPORTS (Très important pour que App.tsx puisse les utiliser)
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
