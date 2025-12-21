f452607954fe8672a5ac0668a9cb9f702771f8fa
gabrielfrezouls-hue <gabriel.frezouls@gmail.com>
2025-12-21 14:00:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
+    // Cela permet au code de lire la clé API sur GitHub et en local
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
-  base: './',
+  base: './', 
 });
