7beb3c008a8458f8ca49f5c521148b5a3d38e185
gabrielfrezouls-hue <gabriel.frezouls@gmail.com>
2026-02-18 14:28:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ TRÈS IMPORTANT : Remplace par le nom EXACT de ton dépôt GitHub entre les slashs
-  base: '/Chaud-devant-Family/', 
-})
+  base: './', // Important pour que le site marche sur GitHub Pages
+});
